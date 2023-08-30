e07d97226ba6bff2ed911e425208c1f3a5838e17
Abdilah Ramadhani <abdilahramadhani06@gmail.com>
2023-08-30 22:36:22 +0300

    Added password hashing

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const {check, validationResult} = require("express-validator");
 const {users} = require("../db");
+const bcrypt = require("bcrypt");
 
 router.post('/signup',[
     check("email","Please provide a valid email").isEmail(),
     check("password","Please provide a password that is longer the 8 characters").isLength({min:6})
-], (req, res)=>{
+], async (req, res)=>{
     const {email, password} = req.body;
 
     const errors = validationResult(req);
@@ -15,24 +16,37 @@ router.post('/signup',[
         })
     }
 
-    let user = users.find((user)=>{
-        return user.email === email
-    });
-
-    if (user){
-        res.status(400).json({
-            "errors": [
-                {
-                    "type": "field",
-                    "value": email,
-                    "msg": "This email address is already taken",
-                    "path": "email",
-                    "location": "body"
-                }
-            ]
+    try {
+        let user = users.find((user)=>{
+            return user.email === email
         });
+
+        if (user){
+            res.status(400).json({
+                "errors": [
+                    {
+                        "type": "field",
+                        "value": email,
+                        "msg": "This email address is already taken",
+                        "path": "email",
+                        "location": "body"
+                    }
+                ]
+            });
+        }
+
+        //hashing a password
+
+        let hashPassword = await bcrypt.hash(password,8);
+
+        users.push({
+            email:email,
+            password:hashPassword
+        });
+
+        res.send(users);
+    } catch (e) {
     }
-    res.send("Validation passed")
 });
 
 module.exports = router;
